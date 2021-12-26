b22c9bd6e0913304dbe01d9d37e3e1efed493ea1
Jordan <personal.jordanabraham@gmail.com>
2021-12-26 15:22:17 -0500

    Merge.

--- src/main/kotlin/TetrisBoard.kt
+++ src/main/kotlin/TetrisBoard.kt
@@ -10,17 +10,13 @@ object TetrisBoard {
     private val grid = Array(12) { x -> Array(21) { y -> if (x == 0 || x == 11 || y == 20) DARK_GREY else Color.BLACK } }
     private val savedTetrominoes = mutableMapOf<Point, Color>()
 
-    fun collides(x: Int, y: Int): Boolean = Point(x, y) in savedTetrominoes || grid[x][y] != Color.BLACK
-
+    fun get(x: Int, y: Int): Color = grid[x][y]
     fun set(x: Int, y: Int, color: Color) {
         grid[x][y] = color
     }
 
-    fun get(x: Int, y: Int): Color = grid[x][y]
-
+    fun collides(x: Int, y: Int): Boolean = Point(x, y) in savedTetrominoes || grid[x][y] != Color.BLACK
     fun paintTetromino(points: Array<Point>, deltaX: Int, deltaY: Int, color: Color) = points.forEach { set(it.x + deltaX, it.y + deltaY, color) }
-
     fun saveTetromino(points: Array<Point>, deltaX: Int, deltaY: Int, color: Color) = points.forEach { savedTetrominoes[Point(it.x + deltaX, it.y + deltaY)] = color }
-
     fun paintSavedTetrominoes() = savedTetrominoes.forEach { set(it.key.x, it.key.y, it.value) }
 }
